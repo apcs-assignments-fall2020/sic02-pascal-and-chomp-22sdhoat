503ace845ab05c49e3eb98eef2738025db148213
22sdhoat <22sdhoat@athenian.org>
2021-01-19 17:22:46 +0000

    Pascals Triangle

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -1,13 +1,22 @@
 /**
  * Write a description of class Pascal here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Samar Dhoat
+ * @version 1/19/21
  */
-public class Pascal
-{
+public class Pascal {
     public static int[][] pascalTriangle(int n) {
-        // YOUR CODE HERE
-        return null;
+        int arr[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    arr[i][j] = 1;
+                }
+                else {
+                    arr[i][j] = (arr[i-1][j-1] + arr[i-1][j]);
+                }
+            }
+        }
+        return arr;
     }
 }
